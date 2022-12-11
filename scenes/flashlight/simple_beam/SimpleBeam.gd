6ae3a3b96a8bf47fcd9d954e8755e75e6a3ff3f8
MojitoMonday <joespicola@gmail.com>
2022-12-11 02:44:34 -0500

    Add flashlight signal

--- scenes/flashlight/simple_beam/SimpleBeam.gd
+++ scenes/flashlight/simple_beam/SimpleBeam.gd
@@ -1,8 +1,13 @@
 extends Area2D
+signal flashlight_hit
 
 func _ready():
 	$SimpleBeamAnimatedSprite.play()
 
 
-func _process(delta):
+func _process(_delta):
 	pass
+
+
+func _on_SimpleBeam_body_entered(_body):
+	emit_signal("flashlight_hit")
