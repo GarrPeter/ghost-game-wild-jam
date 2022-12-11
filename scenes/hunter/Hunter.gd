775da7b123950a5940c12bb001bb7fed78905e0e
MojitoMonday <joespicola@gmail.com>
2022-12-10 23:44:02 -0500

    Added basic player movement

--- scenes/hunter/Hunter.gd
+++ scenes/hunter/Hunter.gd
@@ -1,16 +1,36 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var speed = 300
+var screen_size
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	move(delta)
+
+func move(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x += -1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += -1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	var x_adjust = $HunterSprite.texture.get_width()/2
+	var y_adjust = $HunterSprite.texture.get_height()/2
+	position.x = clamp(position.x, x_adjust, screen_size.x - x_adjust)
+	position.y = clamp(position.y, y_adjust, screen_size.y - y_adjust)
+	
+
