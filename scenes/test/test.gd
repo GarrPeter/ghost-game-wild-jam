a24a63e49452c58bf40693750b7c12cc699cffbd
MojitoMonday <joespicola@gmail.com>
2022-12-12 21:12:58 -0500

    add rotation logic to maintain rotation

--- scenes/test/test.gd
+++ scenes/test/test.gd
@@ -3,8 +3,6 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	initialize_hunter_signals()
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,10 +12,11 @@ func initialize_hunter_signals():
 	$Hunter.connect("flashlight_hit", self, "_on_flashlight_hit")
 	$Hunter.connect("flashlight_exit", self, "_on_flashlight_exit")
 
-func transfer_owner(child, old_parent, new_parent, new_position):
+func transfer_owner(child, old_parent, new_parent, new_position, new_rotation):
 	if child.get_parent() == old_parent:
 		old_parent.remove_child(child)
 		child.position = new_position
+		child.rotation = new_rotation
 		new_parent.call_deferred("add_child", child)
 		yield(get_tree(), "idle_frame")
 
@@ -29,11 +28,12 @@ func _on_flashlight_hit(body):
 				self,
 				$Hunter, 
 				Vector2($Hunter.flashlight_offset.x,
-						$Hunter.to_local(body.position).y))
+						$Hunter.to_local(body.position).y),
+				body.rotation - $Hunter.rotation)
 			body.freeze()
 
 func _on_flashlight_exit(body):
 	if body is WhiteGhost and body.get_parent() == $Hunter:
 		if body.frozen:
-			transfer_owner(body, $Hunter, self, body.get_global_position())
+			transfer_owner(body, $Hunter, self, body.get_global_position(), body.original_rotation)
 			body.unfreeze()
