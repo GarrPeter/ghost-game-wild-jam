da2f8a7b0a7d8053b1048585e78c1e9d27c1a276
MojitoMonday <joespicola@gmail.com>
2022-12-12 21:01:42 -0500

    Add basic dragging logic

--- scenes/test/test.gd
+++ scenes/test/test.gd
@@ -14,22 +14,26 @@ func initialize_hunter_signals():
 	$Hunter.connect("flashlight_hit", self, "_on_flashlight_hit")
 	$Hunter.connect("flashlight_exit", self, "_on_flashlight_exit")
 
-func transfer_owner(child, old_parent, new_parent, new_x, new_y):
+func transfer_owner(child, old_parent, new_parent, new_position):
 	if child.get_parent() == old_parent:
 		old_parent.remove_child(child)
+		child.position = new_position
 		new_parent.call_deferred("add_child", child)
 		yield(get_tree(), "idle_frame")
 
 func _on_flashlight_hit(body):
 	if body is WhiteGhost:
-		if not body.frozen: # and body.get_parent() != $Hunter:
+		if not body.frozen and body.get_parent() != $Hunter:
+			transfer_owner(
+				body,
+				self,
+				$Hunter, 
+				Vector2($Hunter.flashlight_offset.x,
+						$Hunter.to_local(body.position).y))
 			body.freeze()
-#			transfer_owner(body, self, $Hunter, 0, $Hunter.position.y - body.position.y)
-
 
 func _on_flashlight_exit(body):
-	if body is WhiteGhost: # and body.get_parent() == $Hunter:
+	if body is WhiteGhost and body.get_parent() == $Hunter:
 		if body.frozen:
+			transfer_owner(body, $Hunter, self, body.get_global_position())
 			body.unfreeze()
-#			transfer_owner(body, $Hunter, self, body.get_global_position().x, body.get_global_position().y)
-			
